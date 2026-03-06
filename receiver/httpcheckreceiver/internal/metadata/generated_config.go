a6ec90a50e4e1e505934d894355b615475960630
Tina Haiser <tinahaiser@gmail.com>
2026-03-06 08:09:59 +0100

    run make generate

--- receiver/httpcheckreceiver/internal/metadata/generated_config.go
+++ receiver/httpcheckreceiver/internal/metadata/generated_config.go
@@ -11,9 +11,8 @@ import (
 
 // MetricConfig provides common config for a particular metric.
 type MetricConfig struct {
-	Enabled          bool `mapstructure:"enabled"`
-	enabledSetByUser bool
-
+	Enabled             bool `mapstructure:"enabled"`
+	enabledSetByUser    bool
 	AggregationStrategy string   `mapstructure:"aggregation_strategy"`
 	EnabledAttributes   []string `mapstructure:"attributes"`
 	definedAttributes   []string
@@ -29,23 +28,25 @@ func (ms *MetricConfig) Unmarshal(parser *confmap.Conf) error {
 	if err != nil {
 		return err
 	}
-	for _, val := range ms.EnabledAttributes {
-		if !slices.Contains(ms.definedAttributes, val) {
-			return fmt.Errorf("%v is not defined in metadata.yaml", val)
+	if len(ms.definedAttributes) > 0 {
+		for _, val := range ms.EnabledAttributes {
+			if !slices.Contains(ms.definedAttributes, val) {
+				return fmt.Errorf("%v is not defined in metadata.yaml", val)
+			}
 		}
-	}
 
-	for _, val := range ms.requiredAttributes {
-		if !slices.Contains(ms.EnabledAttributes, val) {
-			return fmt.Errorf("`attributes` field must contain required attribute: %v", val)
+		for _, val := range ms.requiredAttributes {
+			if !slices.Contains(ms.EnabledAttributes, val) {
+				return fmt.Errorf("`attributes` field must contain required attribute: %v", val)
+			}
 		}
-	}
 
-	if ms.AggregationStrategy != AggregationStrategySum &&
-		ms.AggregationStrategy != AggregationStrategyAvg &&
-		ms.AggregationStrategy != AggregationStrategyMin &&
-		ms.AggregationStrategy != AggregationStrategyMax {
-		return fmt.Errorf("invalid aggregation strategy set: '%v'", ms.AggregationStrategy)
+		if ms.AggregationStrategy != AggregationStrategySum &&
+			ms.AggregationStrategy != AggregationStrategyAvg &&
+			ms.AggregationStrategy != AggregationStrategyMin &&
+			ms.AggregationStrategy != AggregationStrategyMax {
+			return fmt.Errorf("invalid aggregation strategy set: '%v'", ms.AggregationStrategy)
+		}
 	}
 
 	ms.enabledSetByUser = parser.IsSet("enabled")
@@ -76,100 +77,76 @@ type MetricsConfig struct {
 func DefaultMetricsConfig() MetricsConfig {
 	return MetricsConfig{
 		HttpcheckClientConnectionDuration: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url", "network.transport"},
-			EnabledAttributes:   []string{"http.url", "network.transport"},
+			Enabled: false, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url", "network.transport"},
+			EnabledAttributes:  []string{"http.url", "network.transport"},
 		},
 		HttpcheckClientRequestDuration: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckDNSLookupDuration: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckDuration: MetricConfig{
-			Enabled: true,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: true, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckError: MetricConfig{
-			Enabled: true,
-
-			AggregationStrategy: AggregationStrategySum,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url", "error.message"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: true, AggregationStrategy: AggregationStrategySum,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url", "error.message"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckResponseDuration: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckResponseSize: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckStatus: MetricConfig{
-			Enabled: true,
-
-			AggregationStrategy: AggregationStrategySum,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url", "http.status_code", "http.method", "http.status_class"},
-			EnabledAttributes:   []string{"http.url", "http.status_code", "http.method", "http.status_class"},
+			Enabled: true, AggregationStrategy: AggregationStrategySum,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url", "http.status_code", "http.method", "http.status_class"},
+			EnabledAttributes:  []string{"http.url", "http.status_code", "http.method", "http.status_class"},
 		},
 		HttpcheckTLSCertRemaining: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url", "http.tls.issuer", "http.tls.cn", "http.tls.san"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url", "http.tls.issuer", "http.tls.cn", "http.tls.san"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckTLSHandshakeDuration: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategyAvg,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategyAvg,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckValidationFailed: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategySum,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url", "validation.type"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategySum,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url", "validation.type"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 		HttpcheckValidationPassed: MetricConfig{
-			Enabled: false,
-
-			AggregationStrategy: AggregationStrategySum,
-			requiredAttributes:  []string{},
-			definedAttributes:   []string{"http.url", "validation.type"},
-			EnabledAttributes:   []string{"http.url"},
+			Enabled: false, AggregationStrategy: AggregationStrategySum,
+			requiredAttributes: []string{},
+			definedAttributes:  []string{"http.url", "validation.type"},
+			EnabledAttributes:  []string{"http.url"},
 		},
 	}
 }
